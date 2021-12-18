4be627a91a674b95d339bd95abc603ef7ffb6dce
NguyenLoi851 <60845268+NguyenLoi851@users.noreply.github.com>
2021-12-18 16:01:37 +0700

    Update h.cpp

--- Applied Algorithm/Codeforce/Training 1/h.cpp
+++ Applied Algorithm/Codeforce/Training 1/h.cpp
@@ -1,26 +1,50 @@
+// #include<iostream>
+// #include<vector>
+// #include<iterator>
+// #include<algorithm>
+// using namespace std;
+
+// int main(){
+//     int n, m, temp, res =1;
+//     cin >> n;
+//     vector<int> v;
+//     for(int i=0; i<n; i++){
+//         cin >> temp;
+//         v.push_back(temp);
+//     }
+//     cin >> m;
+//     for(int i=0;i<m; i++){
+//         cin>>temp;
+//         vector<int>::iterator it = find(v.begin(),v.end(),temp);
+//         if(it==v.end()){
+//             res = 0;
+//             break;
+//         }
+//     }
+//     cout << res; 
+//     return 0;
+// }
+
 #include<iostream>
-#include<vector>
-#include<iterator>
-#include<algorithm>
 using namespace std;
 
 int main(){
-    int n, m, temp, res =1;
+    int n,m,a=0,b=0,tmp,res=1;
     cin >> n;
-    vector<int> v;
-    for(int i=0; i<n; i++){
-        cin >> temp;
-        v.push_back(temp);
+    for(int i=0;i<n;i++){
+        cin >> tmp;
+        a = a | (1<<tmp);
     }
     cin >> m;
-    for(int i=0;i<m; i++){
-        cin>>temp;
-        vector<int>::iterator it = find(v.begin(),v.end(),temp);
-        if(it==v.end()){
-            res = 0;
-            break;
+    for(int i=0;i<m;i++){
+        cin >> tmp;
+        if(a & (1<<tmp)){
+            continue;
+        }else{
+            res=0;
         }
     }
-    cout << res; 
+    cout << res;
     return 0;
 }
+
